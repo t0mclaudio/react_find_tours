e85d6588e38747341b804b4b60a7eac92f653122
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-10-12 15:16:09 +0800

    Add search results component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBox from './components/SearchBox'
+import SearchResults from './components/SearchResults'
 import data from './data'
 
 class App extends Component {
@@ -8,6 +9,8 @@ class App extends Component {
       <div className="App">
         <h1>Hello</h1>
         <SearchBox />
+        <SearchResults data={data} />
+
       </div>
     );
   }
