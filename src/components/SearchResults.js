0b89235f6e4e851f72d26b26515a6fde8cd8d5f0
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-10-12 16:45:17 +0800

    Integrate not found when search returns 0

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import Package from './Package'
+import Package from './Package';
+import NotFound from './notFound';
 
 const SearchResults = props => {
-    return (
-        <div id="SearchResults">
-            {props.data.map((tour) => {
-                return (
-                    <Package key={tour.code} tour={tour} />
-                )
-            })}
-        </div>     
-    )
+    if (props.found > null) {
+        return (
+            <div id="SearchResults">
+                {props.data.map((tour) => {
+                    return (
+                        <Package key={tour.code} tour={tour} />
+                    )
+                })}
+            </div>     
+        )
+    } else {
+        return (
+            <NotFound />
+        )
+    }
 }
 export default SearchResults;
