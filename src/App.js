61459c0d878c2a93bcd544aeb91aa2736cc978bf
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-10-12 13:52:50 +0800

    Import searchbox component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SearchBox from './components/SearchBox'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <SearchBox />
       </div>
     );
   }
