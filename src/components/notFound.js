c73d65c5caa487d5be4d837ba9042398fab6459d
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-10-12 16:44:57 +0800

    Create not found module

---
+++ src/components/notFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = props => {
+    return (
+        <div>
+            <h2 className="notFound">Ooops!!! We did not find anything</h2>
+        </div>
+    )
+}
+
+export default NotFound;
