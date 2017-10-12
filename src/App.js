c4cb598e525da14922f0be81a92f2566b425b5b4
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-10-12 16:18:36 +0800

    Create method to filter based on search valeu

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import SearchResults from './components/SearchResults'
 import data from './data'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {tours: data}
+  }
+  onType(e) {
+    var filtered = []
+    data.map((obj)=>{
+      let name = obj.name.toLowerCase()
+      let keyword = e.target.value.toLowerCase()
+      if (name.includes(keyword)){
+        filtered.push(obj)
+      }
+    })
+    this.setState({tours: filtered})
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <SearchBox />
-        <SearchResults data={data} />
-
+        <SearchBox onType={(e) => this.onType(e) } />
+        <SearchResults data={this.state.tours} />
       </div>
     );
   }
