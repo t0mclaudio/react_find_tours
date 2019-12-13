6990d7e4bbe20f274e1a74ef1a1f5e82f0603e19
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-13 20:06:41 +0800

    Use React Hooks - replace Stateful component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import SearchBox from './components/SearchBox'
 import SearchResults from './components/SearchResults'
 import data from './data'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tours: data,
-      filtered: data,
-      filtet: null,
-    }
-    this.onType = this.onType.bind(this);
+const App = () => {
+  const [tours] = useState(data);
+  const [filtered, setFiltered] = useState(tours);
+  const [found, setFound] = useState(filtered.length);
+
+  const onType = (e) => {
+    let keyword = e.target.value.toLowerCase();
+    let filtered_by = tours.filter(tour => tour.name.toLowerCase().includes(keyword))
+    setFiltered(filtered_by);
+    setFound(filtered_by.length)
   }
 
-  componentDidMount() {
-    this.setState({
-      found: this.state.filtered.length
-    })
-  }
-
-  onType(e) {
-    let filter = e.target.value.toLowerCase();
-    let filtered = this.state.tours.filter(tour => tour.name.toLowerCase().includes(filter));
-    let found = filtered.length;
-    this.setState({
-      filter: filter,
-      filtered: filtered,
-      found: found
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Find me a tour</h1>
-        <SearchBox onType={this.onType} />
-        <SearchResults data={this.state.filtered} found={this.state.found} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Find me a tour</h1>
+      <SearchBox onType={e => onType(e)} />
+      <SearchResults data={filtered} found={found} />
+    </div>
+  );
 }
 
 export default App;
