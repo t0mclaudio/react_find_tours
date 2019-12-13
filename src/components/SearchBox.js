0efc27b4ccaa0be5258d40017eaf04c8b5152fb6
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-13 17:48:45 +0800

    Remove value props

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,6 @@ const SearchBox = props => {
     return (
         <input
             onChange={e => props.onType(e)}
-            value={props.value} 
             type="text"
             placeholder="Enter a keyword"
         />
